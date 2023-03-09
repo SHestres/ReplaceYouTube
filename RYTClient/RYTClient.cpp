15e8b8d71870acd4264d2249389b9db7a7069175
SHestres <sidneyhestres@gmail.com>
2023-03-09 16:25:53 -0600

    Geeks4Geeks doesn't know what's up either

--- RYTClient/RYTClient.cpp
+++ RYTClient/RYTClient.cpp
@@ -1,31 +1,69 @@
 // ReplaceYouTube.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-#include <net>
-#include <string_view>
+// Server side C/C++ program to demonstrate Socket
+// programming
 
-int main()
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+int main(int argc, char const* argv[])
 {
-    std::cout << "Connecting to server..." << std::endl;
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = { 0 };
+    char* hello = "Hello from server";
 
-    std::net::io_context context;
-    std::net::ip::tcp::resolver resolver(context);
-    std::net::ip::tcp::socket socket(context);
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
 
-    auto endpoints = resolver.resolve("localhost", "12345");
-    std::net::connect(socket, endpoints);
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET,
+        SO_REUSEADDR | SO_REUSEPORT, &opt,
+        sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-    std::string message;
-    message.resize(128);
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr*)&address,
+        sizeof(address))
+        < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket
+        = accept(server_fd, (struct sockaddr*)&address,
+            (socklen_t*)&addrlen))
+        < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read(new_socket, buffer, 1024);
+    printf("%s\n", buffer);
+    send(new_socket, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
 
-    std::net::read(socket, std::net::buffer(message));
-
-    std::cout << "Received message from server: " << message << std::endl;
-
-    socket.shutdown(std::net::socket_base::shutdown_both);
-    socket.close();
-
+    // closing the connected socket
+    close(new_socket);
+    // closing the listening socket
+    shutdown(server_fd, SHUT_RDWR);
     return 0;
 }
 
