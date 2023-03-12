089727c6030f0df2a23087e0000fb6f78c744246
SHestres <sidneyhestres@gmail.com>
2023-03-12 15:36:58 -0500

    Quicksave: Waiting for a connection

--- RYTServer/RYTServer.cpp
+++ RYTServer/RYTServer.cpp
@@ -1,5 +1,5 @@
 // RYTServer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Following tutorial at https://www.youtube.com/watch?v=WDn-htpBlnU
 
 
 #include <iostream>
@@ -13,14 +13,50 @@
 void main()
 {
 	//Initialize winsock
+	WSADATA wsData; //Struct that holds config data
+	WORD ver = MAKEWORD(2, 2); //Use version 2.2
+
+	int wsOK = WSAStartup(ver, &wsData);
+	if (wsOK != 0)
+	{
+		std::cerr << "Can't initialize Winsock. Exiting" << std::endl;
+		return;
+	}
 
 	//Create a socket
+	/*
+		A socket is often just a number that get's mapped in the OS, but on windows systems
+		it's given an actual type
+	*/
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0); //(Address family, Socket type (stream for tcp), protocol flag)
+	if (listening == INVALID_SOCKET)
+	{
+		std::cerr << "Can't create socket. Exiting" << std::endl;
+		return;
+	}
 
-	//Bind the socket to an ip address and port
+	//Bind an ip address and port to the socket
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY; //Could also use inet_pton ....
+
+	bind(listening, (sockaddr*)&hint, sizeof(hint)); 
 
 	//Tell Winsock the socket is for listening
+	listen(listening, SOMAXCONN); //Marks socket for listening, doesn't actually accept a connection
 
 	//Wait for a connection
+	sockaddr_in client;
+	int clientSize = sizeof(client);
+
+	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		std::cerr << "Client socket invalid." << std::endl;
+	}
+
+
 
 	//Close listening socket
 
