e3b49e4c4f22c7bb88818b46b9bf5e34e6c4a9bc
SHestres <sidneyhestres@gmail.com>
2023-03-30 18:26:08 -0500

    Basic Window (Last commit before switching to imgui)

--- RYTClient/AppWindow.cpp
+++ RYTClient/AppWindow.cpp
@@ -59,20 +59,31 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
-    case WM_DESTROY:
-        PostQuitMessage(0);
+        /*case WM_CLOSE:
+        {
+            if (MessageBox(hwnd, L"Really quit?", L"My application", MB_OKCANCEL) == IDOK)
+            {
+                DestroyWindow(hwnd);
+            }
+            // Else: User canceled. Do nothing.
+            return 0;
+        }*/
+        case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+            return 0;
+        }
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            // All painting occurs here, between BeginPaint and EndPaint.
+            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));  //Part of GDI, which has been replaced by Direct2D (it's still supported though)
+        
+            EndPaint(hwnd, &ps);
+        }
         return 0;
-
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-
-        // All painting occurs here, between BeginPaint and EndPaint.
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        EndPaint(hwnd, &ps);
-    }
-    return 0;
     }
 
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
