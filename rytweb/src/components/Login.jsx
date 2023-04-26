43031ab56d4acc701e6ac09dcaf2c2f2441d516f
SHestres <sidneyhestres@gmail.com>
2023-04-26 12:07:08 -0500

    Login page complete

--- rytweb/src/components/Login.jsx
+++ rytweb/src/components/Login.jsx
@@ -1,10 +1,17 @@
 import Layout from './Layout'
+import bgImg from '../assets/sign-in-background.jpg'
 
 export default function Login()
 {
     return(
         <Layout>
-            <h1>These are the children</h1>
+            <div className="login-panel">
+                <h1>Sign In</h1>
+                <input type='text' placeholder='Username'></input>
+                <input type='text' placeholder='Password'></input>
+                <button id='signInButton'>Sign In</button>
+            </div>
+            <img className="login-bg" src={bgImg}></img>
         </Layout>
     )
 }
