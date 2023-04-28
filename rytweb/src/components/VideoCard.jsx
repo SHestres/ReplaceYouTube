268ba7bd6fc42152cad2c8783cb5e06b6641a8e4
SHestres <sidneyhestres@gmail.com>
2023-04-27 19:10:34 -0500

    Video images match size, and auto scale width

--- rytweb/src/components/VideoCard.jsx
+++ rytweb/src/components/VideoCard.jsx
@@ -1,16 +1,49 @@
-
+import { useState, useEffect } from "react";
+import {uniqueId} from 'lodash'
 
 export default function VideoCard({Title, id, vote_average, poster, Images }) {
 
-    let style = {
-        backgroundImage: `url(${Images[0]})`,//`url(/images${imgSrc})`;
-        backgroundSize: 'contain',
-        backgroundRepeat: 'no-repeat',
-        width: '100%',
+    const [style, setStyle] = useState({});
 
-    }
+    const getMeta = (url, cb) => {
+        const img = new Image();
+        img.onload = () => cb(null, img);
+        img.onerror = (err) => cb(err);
+        img.src = url;
+      };
 
-    return <div className="video-card" style={style}>
+    let backHeight = 1;
+    let backWidth = 1;
+
+    getMeta(`${Images[0]}`, (err, img) =>
+    {
+        console.log("Updating image ratio");
+        backWidth = img.naturalWidth;
+        backHeight = img.naturalHeight;
+    })
+    
+    useEffect(() =>
+    {
+        setStyle({
+            backgroundImage: `url(${Images[0]})`,//`url(/images${imgSrc})`;
+            backgroundSize: 'auto 100%',
+            backgroundRepeat: 'no-repeat',
+            height: '100%',
+            aspectRatio: `${backWidth} / ${backHeight}`,
+            flexShrink: 0,
+            //position: 'relative',
+            backgroundPosition: 'center',
+            //width: 'auto',
+            //display: 'flex',
+            //alignItems: 'flex-end',
+            border: '1px solid red'
+        })
+    }, [backHeight, backWidth])
+
+    const tagId = uniqueId();
+    console.log(tagId);
+
+    return <div className="video-card" key={tagId} style={style}>
         <div className="video-details">
             <h4>{Title}</h4>
         </div>
