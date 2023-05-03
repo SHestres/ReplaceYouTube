d135eaa498d909edd8ef78266e79edd7af45f5bb
SHestres <sidneyhestres@gmail.com>
2023-05-02 21:16:45 -0500

    Fixed undefined obj error

--- rytweb/src/components/Browser.jsx
+++ rytweb/src/components/Browser.jsx
@@ -1,13 +1,13 @@
 import GenreCollection from './GenreCollection';
 import Layout from './Layout'
 import { useContext, useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom/';
-import useGlobalState from '../services/useGlobalState';
+import { useParams } from 'react-router-dom';
 
 export default function Browser(){
     const [genres, setGenres] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [hasGenre, setHasGenre] = useState(false);
+    const [cats, setCats] = useState();
     const {category} = useParams();
 
     //Allows things to be run when a sensitivity list changes
@@ -21,7 +21,7 @@ export default function Browser(){
         .catch(() => console.log("Error getting genres"));
     }, []) //Empyt arr as second param means it will run once when Browser() is run (loaded)
 
-    const [cats, setCats] = useState();
+    
 
     useEffect(()=>{
         fetch(`http://${window.location.host.split(":", 1)[0]}:5100/api/list/categories`)
@@ -52,7 +52,7 @@ export default function Browser(){
             {
                 hasGenre ? null: (
                     <>
-                        <h2 className='movie-category-label'>{cats[category]}</h2>
+                        {cats ? <h2 className='movie-category-label'>{cats[category]}</h2> : null}
                         <div className="no-show-text">Add videos for them to show up here!</div>
                     </>
                 )
